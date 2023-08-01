d268176b5020d40219a31230a4bf888227883320
Habib Talib <cyberx11@gmail.com>
2023-08-01 12:49:53 +0800

    add treatment relation in patience resource

--- app/Filament/Resources/PatientResource.php
+++ app/Filament/Resources/PatientResource.php
@@ -90,7 +90,7 @@ class PatientResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\TreatmentsRelationManager::class,
         ];
     }
 
